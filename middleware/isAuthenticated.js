b667fdfc24348caf3606500067a0eaaea1eba6d7
Rahul_Pharthyal_MNNLR <mern.mnnlr.rahulpharthyal@gmail.com>
2024-09-18 19:59:54 +0530

    change on authaction line 47

--- middleware/isAuthenticated.js
+++ middleware/isAuthenticated.js
@@ -14,6 +14,7 @@ const isAuthenticated = async (req, res, next) => {
     req.user = decoded;
     next();
   } catch (err) {
+    console.log(err);
     return res.status(401).json({
       success: false,
       message: "Unauthorised User!",
