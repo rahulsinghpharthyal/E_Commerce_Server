2c092ecf1276a4a60e4a87357c2a8ba181327e14
Rahul Pharthyal <rahulpharthyal04@gmail.com>
2024-12-01 02:47:10 +0530

    add same site attribute on cookie

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -64,7 +64,7 @@ const loginUser = async (req, res) => {
     res
       .cookie("token", token, {
         httpOnly: true,
-        secure: false,
+        secure: true,
         sameSite: 'none'
       })
       .json({
